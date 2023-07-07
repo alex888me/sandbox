48e47568f5ab3dd5ef042954e7e00fb2bbc7d30d
art <art971m@gmail.com>
2023-07-07 11:35:51 +0200

    check_id_unix_user.sh

--- shell/check_id_unix_user.sh
+++ shell/check_id_unix_user.sh
@@ -2,9 +2,12 @@
 LOGFILE="/path/to/your/logfile.log" # Replace this with the path to your log file.
 
 while true; do
-  OUTPUT=$(id user 2>&1)
-  EXITCODE=$?
-  echo "$(date); Exit code: ${EXITCODE}; ${OUTPUT}" >> $LOGFILE
+  TIMEFORMAT="%R " # Setting time format and preventing newline
+  OUTPUT=$( { time id user; } 2>&1) # Running the command and storing the output.
+  EXITCODE=$? # Storing the exit code of the last run command.
+  echo -n "$(date): " >> $LOGFILE
+  echo -n "${OUTPUT}, " >> $LOGFILE
+  echo "Exit code: ${EXITCODE}" >> $LOGFILE
   sleep 5
 done
 
